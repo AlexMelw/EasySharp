ca590fa5aeeecef5005fb26cd1b40bd3f1db8cd7
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-04-18 20:21:24 +0300

    Update DictionaryHelper.cs

--- NHelpers/DictionaryHelper.cs
+++ NHelpers/DictionaryHelper.cs
@@ -1,40 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasySharp.NHelpers
 {
     public static class DictionaryHelper
     {
-        public static void computeIfAbsent<TKey, TValue>(this Dictionary<TKey, TValue> dictionary,
-            TKey key, Func<TKey, List<TValue>> mappingFunc)
+        /// <summary>
+        /// Initializes value (collection of type <typeparamref name="TElement"/>) if it doesn't exist and maps it to the given <paramref name="key"/>
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// foreach (Employee employee in employeesCollection)
+        /// {
+        ///     dictionary.MapCollectionToKey(key, () =&gt; new List&lt;Employee&gt;());
+        ///     dictionary[key].add(employee);
+        /// }
+        /// </code>
+        /// instead of
+        /// <code>
+        /// foreach (Employee employee in employeesCollection){
+        ///     if (dictionary[key] == null)
+        ///         dictionary.put(key), new List&lt;Employee&gt;());
+        ///     
+        ///     dictionary[key].add(employee);
+        /// }
+        /// </code>
+        /// </example>
+        /// <typeparam name="TKey">Type of key</typeparam>
+        /// <typeparam name="TValue">Type of value (that is <see cref="ICollection{T}"/>)</typeparam>
+        /// <typeparam name="TElement">Type of elements in the <typeparamref name="TValue"/> collection</typeparam>
+        /// <param name="dictionary">source dictionary</param>
+        /// <param name="key">to which will be mapped (if isn't already) a value of type <see cref="ICollection{T}"/> where T is <typeparamref name="TElement"/></param>
+        /// <param name="mappingFunc">Function that performs value (<see cref="ICollection{T}"/>) mapping to the given key</param>
+        public static void MapCollectionToKey<TKey, TValue, TElement>(
+            this Dictionary<TKey, TValue> dictionary,
+            TKey key,
+            Func<ICollection<TElement>> mappingFunc)
+            where TValue : ICollection<TElement>
         {
-            // JAVA CODE
-            //if ((v = get(key)) == null)
-            //{
-            //    V newValue;
-            //    if ((newValue = mappingFunc.apply(key)) != null)
-            //    {
-            //        put(key, newValue);
-            //    }
-            //}
+            if (!dictionary.ContainsKey(key))
+            {
+                var value = (TValue) mappingFunc();
+                if (value != null)
+                    dictionary.Add(key, value);
+            }
         }
-
-        // USAGE
-        //for (Student student: studentsCollection){
-        //     map.computeIfAbsent(keySpecification.getComparisionKey(student), k -> new ArrayList<>());
-        //     map.get(keySpecification.getComparisionKey(student)).add(student);
-        //}
-
-        // instead of
-
-        //for (Student student: studentsCollection){
-        //   if (map.get(keySpecification.getComparisionKey(student)) == null){
-        //       map.put(keySpecification.getComparisionKey(student), new ArrayList<>());
-        //   }
-        //   map.get(keySpecification.getComparisionKey(student)).add(student);
-        //}
     }
 }
