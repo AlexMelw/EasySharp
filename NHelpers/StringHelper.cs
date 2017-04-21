9c620c5559cb2899e4d06eb88b43974aa6534a1a
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-04-22 01:18:27 +0300

    Add Base64 Extension Methods

--- NHelpers/StringHelper.cs
+++ NHelpers/StringHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace EasySharp.NHelpers
 {
@@ -84,10 +85,10 @@ namespace EasySharp.NHelpers
             {
                 indexPosition = source.IndexOf(subString, indexPosition, mode);
 
-                // searchKey isn't contained by the given substring (indexPosition..source.Length-1)
+                // searchKey isn't contained by the given substring (indexPosition..plainTextSource.Length-1)
                 if (indexPosition == -1) break;
 
-                // searchKey IS contained by the given substring (indexPosition..source.Length-1)
+                // searchKey IS contained by the given substring (indexPosition..plainTextSource.Length-1)
                 ++occurencesCount;
             }
 
@@ -99,22 +100,51 @@ namespace EasySharp.NHelpers
         ///     <see cref="string" />
         /// </summary>
         /// <param name="source">
-        ///     <see cref="string" />s collection source that will be aggregated into a single comma-separated
+        ///     <see cref="string" />s collection plainTextSource that will be aggregated into a single comma-separated
         ///     <see cref="string" />
         /// </param>
         /// <returns>Comma separated string</returns>
-        public static string CommaSeparatedString(this IEnumerable<string> source) => source.Aggregate(
-            (s1, s2) => $"{s1}, {s2}");
+        public static string CommaSeparatedString(this IEnumerable<string> source)
+        {
+            return source.Aggregate((s1, s2) => $"{s1}, {s2}");
+        }
 
         /// <summary>
-        ///     Aggregates the <paramref name="source" /> that is a collection of variable number of arguments or an array of arguments of 
+        ///     Aggregates the <paramref name="source" /> that is a collection of variable number of arguments or an array of
+        ///     arguments of
         ///     <see cref="string" /> type.
         /// </summary>
         /// <param name="source">
-        ///     <see cref="string" />s collection source that will be aggregated into a single comma-separated
+        ///     <see cref="string" />s collection plainTextSource that will be aggregated into a single comma-separated
         ///     <see cref="string" />
         /// </param>
         /// <returns>Comma separated string</returns>
-        public static string CommaSeparatedString(params string[] source) => source.CommaSeparatedString();
+        public static string CommaSeparatedString(params string[] source)
+        {
+            return source.CommaSeparatedString();
+        }
+
+        /// <summary>
+        ///     Decodes the encoded <paramref name="base64EncodedDataSource" /> by Base64 (MIME) scheme.
+        /// </summary>
+        /// <remarks>You should use Base64 whenever you intend to transmit binary data in a textual format.</remarks>
+        /// <param name="base64EncodedDataSource">Encoded <see cref="string" /> by Base64 scheme.</param>
+        /// <returns></returns>
+        public static string GetStringDecodedFromBase64String(this string base64EncodedDataSource)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(base64EncodedDataSource));
+        }
+
+
+        /// <summary>
+        ///     Encodes <paramref name="plainTextSource" /> to the Base64 (MIME)
+        /// </summary>
+        /// <remarks>You should use Base64 whenever you intend to transmit binary data in a textual format.</remarks>
+        /// <param name="plainTextSource">A plain UTF8 encoded <see cref="string" /></param>
+        /// <returns></returns>
+        public static string ToBase64String(this string plainTextSource)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(plainTextSource));
+        }
     }
 }
