6dac807c5cfb428c34ead3736fcc445034a8d7b1
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-08-03 12:11:28 +0300

    Add an universal Print() extension method

--- NHelpers/CustomExtensionMethods/GenericTypeHelper.cs
+++ NHelpers/CustomExtensionMethods/GenericTypeHelper.cs
@@ -1,5 +1,7 @@
 ﻿namespace EasySharp.NHelpers.CustomExtensionMethods
 {
+    using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -38,6 +40,96 @@
             IEqualityComparer<TSource> comparer)
         {
             return source.Contains(key, comparer);
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
+        ///         the text string or stream.
+        ///     </para>
+        ///     <para>
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as a JS-like array
+        ///         representation.
+        ///     </para>
+        /// </summary>
+        /// <param name="value">
+        ///     The object to be written on console. If <paramref name="value" /> is <see langword="null" />, only
+        ///     the line terminator is written.
+        /// </param>
+        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
+        /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
+        public static void Print<TValue>(this TValue value)
+        {
+            if (value is IEnumerable enumerable)
+            {
+                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
+                Console.Out.WriteLine(strings.ToJsArrayRepresentation());
+            }
+            else
+            {
+                Console.Out.WriteLine(value);
+            }
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
+        ///         the text string or stream.
+        ///     </para>
+        ///     <para>
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as sequence of
+        ///         comma-separated items.
+        ///     </para>
+        /// </summary>
+        /// <param name="value">
+        ///     The object to be written on console. If <paramref name="value" /> is <see langword="null" />, only
+        ///     the line terminator is written.
+        /// </param>
+        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
+        /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
+        public static void PrintAsCommaSeparatedString<TValue>(this TValue value)
+        {
+            if (value is IEnumerable enumerable)
+            {
+                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
+                Console.Out.WriteLine(strings.ToCommaSeparatedString());
+            }
+            else
+            {
+                Console.Out.WriteLine(value);
+            }
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
+        ///         the text string or stream.
+        ///     </para>
+        ///     <para>
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as sequence of
+        ///         comma-separated items ending with a dot.
+        ///     </para>
+        /// </summary>
+        /// <param name="value">
+        ///     The object to be written on console. If <paramref name="value" /> is <see langword="null" />, only
+        ///     the line terminator is written.
+        /// </param>
+        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
+        /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
+        public static void PrintAsCommaSeparatedStringWithEndingDot<TValue>(this TValue value)
+        {
+            if (value is IEnumerable enumerable)
+            {
+                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
+                Console.Out.WriteLine(strings.ToCommaSeparatedStringWithEndingDot());
+            }
+            else
+            {
+                Console.Out.WriteLine($"{value}.");
+            }
         }
     }
 }
