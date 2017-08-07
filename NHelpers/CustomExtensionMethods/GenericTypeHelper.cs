750a61980a1862d172f6d98d0448a9f89bc0c12f
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-08-07 16:23:56 +0300

    Make Print() extension method even smarter.

--- NHelpers/CustomExtensionMethods/GenericTypeHelper.cs
+++ NHelpers/CustomExtensionMethods/GenericTypeHelper.cs
@@ -36,20 +36,28 @@
         ///     <see langword="true" /> if the source sequence contains an element that has the specified value
         ///     <paramref name="key" />; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool In<TSource>(this TSource key, IEnumerable<TSource> source,
-            IEqualityComparer<TSource> comparer)
+        public static bool In<TSource>(this TSource key, IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
         {
             return source.Contains(key, comparer);
         }
 
         /// <summary>
         ///     <para>
-        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
+        ///         Writes on console the parameter <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
         ///         the text string or stream.
         ///     </para>
         ///     <para>
-        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as a JS-like array
-        ///         representation.
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" /> and <typeparamref name="TValue" /> is a
+        ///         primitive type, then it is printed as an array of comma-separated items.
+        ///     </para>
+        ///     <para>
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" /> and <typeparamref name="TValue" /> is
+        ///         <see cref="string" /> type, then it is printed as an array of items wrapped in quotation marks, separated by
+        ///         <see cref="Environment.NewLine" /> and a comma.
+        ///     </para>
+        ///     <para>
+        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" /> and <typeparamref name="TValue" /> is an
+        ///         unknown type, then it is printed as a JS-like array representation.
         ///     </para>
         /// </summary>
         /// <param name="value">
@@ -59,76 +67,73 @@
         /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
         /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
         /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
+        /// <example>
+        ///     <code>777.Print();</code>
+        ///     <code>
+        ///         int x = 55; x.Print();
+        ///     </code>
+        ///     <code>new[] {"Apple", "Not \"Apple", "Foo"}.Print();</code>
+        ///     <code>new[] { 1, 2, 6, 98 }.Print();</code>
+        ///     <code>
+        ///         new[]
+        ///         {
+        ///             new Student
+        ///             {
+        ///                 FirstName = "Alex",
+        ///                 LastName = "Melvin",
+        ///                 AverageMark = 9.54
+        ///             },
+        ///             new Student
+        ///             {
+        ///                 FirstName = "Allen",
+        ///                 LastName = "Poll",
+        ///                 AverageMark = 8.43
+        ///             }
+        ///         }.Print();
+        /// </code>
+        /// </example>
         public static void Print<TValue>(this TValue value)
         {
             if (value is IEnumerable enumerable)
             {
-                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
-                Console.Out.WriteLine(strings.ToJsArrayRepresentation());
-            }
-            else
-            {
-                Console.Out.WriteLine(value);
-            }
-        }
+                IEnumerable<object> objectsCollection = enumerable.Cast<object>();
 
-        /// <summary>
-        ///     <para>
-        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
-        ///         the text string or stream.
-        ///     </para>
-        ///     <para>
-        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as sequence of
-        ///         comma-separated items.
-        ///     </para>
-        /// </summary>
-        /// <param name="value">
-        ///     The object to be written on console. If <paramref name="value" /> is <see langword="null" />, only
-        ///     the line terminator is written.
-        /// </param>
-        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
-        /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
-        /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
-        public static void PrintAsCommaSeparatedString<TValue>(this TValue value)
-        {
-            if (value is IEnumerable enumerable)
-            {
-                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
-                Console.Out.WriteLine(strings.ToCommaSeparatedString());
-            }
-            else
-            {
-                Console.Out.WriteLine(value);
-            }
-        }
+                if (!objectsCollection.Any())
+                    return;
 
-        /// <summary>
-        ///     <para>
-        ///         Writes on console the <paramref name="value" /> as a <see cref="string" />, followed by a line terminator to
-        ///         the text string or stream.
-        ///     </para>
-        ///     <para>
-        ///         If <paramref name="value" /> is a <see cref="IEnumerable{T}" />, then it is printed as sequence of
-        ///         comma-separated items ending with a dot.
-        ///     </para>
-        /// </summary>
-        /// <param name="value">
-        ///     The object to be written on console. If <paramref name="value" /> is <see langword="null" />, only
-        ///     the line terminator is written.
-        /// </param>
-        /// <exception cref="T:System.ObjectDisposedException">The <see cref="T:System.IO.TextWriter" /> is closed. </exception>
-        /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
-        /// <typeparam name="TValue">The type of <paramref name="value" /> parameter.</typeparam>
-        public static void PrintAsCommaSeparatedStringWithEndingDot<TValue>(this TValue value)
-        {
-            if (value is IEnumerable enumerable)
-            {
-                IEnumerable<string> strings = enumerable.Cast<object>().Select(o => o.ToString());
-                Console.Out.WriteLine(strings.ToCommaSeparatedStringWithEndingDot());
+                object firstItem = objectsCollection.First();
+                Type itemType = firstItem.GetType();
+                bool isPrimitive = itemType.IsPrimitive;
+
+                IEnumerable<string> enumerationAsStrings = objectsCollection.Select(o => o.ToString());
+
+                if (isPrimitive)
+                {
+                    Console.Out.WriteLine($"[ {enumerationAsStrings.ToCommaSeparatedString()} ]");
+                    return;
+                }
+                if (itemType == typeof(string))
+                {
+                    string indentation = "    ";
+                    string NewLine = Environment.NewLine;
+
+                    string resultString = enumerationAsStrings.Aggregate(
+                        seed: $"[",
+                        func: (accumulator, item) => $@"{accumulator}{NewLine}{indentation}""{item}"",",
+                        resultSelector: accumulator => $"{accumulator.Substring(0, accumulator.Length - 1)}{NewLine}]");
+
+                    Console.Out.WriteLine(resultString);
+
+                    return;
+                }
+                // ELSE
+                // We deal with an unknown type of items within IEnumerable<T>
+                Console.Out.WriteLine(enumerationAsStrings.ToJsArrayRepresentation());
             }
             else
             {
-                Console.Out.WriteLine($"{value}.");
+                // Single value (not a collection)
+                Console.Out.WriteLine(value);
             }
         }
     }
