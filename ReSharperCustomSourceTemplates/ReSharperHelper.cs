461fa3d663e6103fa52aceb724965d4c85343dde
Veaceslav BARBARII <slava.barbarii@gmail.com>
2018-03-10 21:52:32 +0200

    Add postfix templates

--- ReSharperCustomSourceTemplates/ReSharperHelper.cs
+++ ReSharperCustomSourceTemplates/ReSharperHelper.cs
@@ -8,7 +8,7 @@ namespace EasySharp.ReSharperCustomSourceTemplates
     {
         [SourceTemplate]
         [Macro(Target = "item", Expression = "suggestVariableName()", Editable = 0)]
-        public static void PrintAll<T>(this IEnumerable<T> source) where T : struct 
+        public static void printAll<T>(this IEnumerable<T> source) where T : struct 
         {
             foreach (var item in source)
             {
@@ -19,9 +19,28 @@ namespace EasySharp.ReSharperCustomSourceTemplates
 
         [SourceTemplate]
         [Macro(Target = "lvalue", Expression = "suggestVariableName()", Editable = 0)]
-        public static void Assign<T>(this T source)
+        public static void assign<T>(this T source)
         {
             //$ lvalue = source;
+        }
+
+        [SourceTemplate]
+        public static void isNullOrEmpty(this string source)
+        {
+            //$string.IsNullOrEmpty(source)$END$
+        }
+
+        [SourceTemplate]
+        public static void isNullOrWhiteSpace(this string source)
+        {
+            //$string.IsNullOrWhiteSpace(source)$END$
+        }
+
+        [SourceTemplate]
+        [Macro(Target = "TYPE", Expression = "completeType()", Editable = 0)]
+        public static void As<T>(this T source)
+        {
+            //$(source as TYPE)
         }
     }
 }
